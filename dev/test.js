2a0cf5328f9dd3d16c05d402938cb46663622f14
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-03-25 14:11:53 -0700

    Create new transaction method

--- dev/test.js
+++ dev/test.js
@@ -2,10 +2,24 @@ const Blockchain = require('./blockchain');
 
 const bidcoin = new Blockchain();
 
-bidcoin.createNewBlock(2000, '1ONINA8998DSFHF', '1MONINA8998DSFHF');
+//1.
+// bidcoin.createNewBlock(2000, '1ONINA8998DSFHF', '1MONINA8998DSFHF');
 
-bidcoin.createNewBlock(3000, '2ONINA8998DSFHF', '2MONINA8998DSFHF');
+// bidcoin.createNewBlock(3000, '2ONINA8998DSFHF', '2MONINA8998DSFHF');
 
-bidcoin.createNewBlock(4000, '3ONINA8998DSFHF', '3MONINA8998DSFHF');
+// bidcoin.createNewBlock(4000, '3ONINA8998DSFHF', '3MONINA8998DSFHF');
 
-console.log(bidcoin);
+//2.
+bidcoin.createNewBlock(892348, '1ONINA8998DSFHF','2ONINA8998DSFHF');
+
+bidcoin.createNewTransaction(100, 'ADLAN2ONINA8998DSFHF', 'JULIA2MONINA8998DSFHF');
+
+bidcoin.createNewBlock(999999, '2ONINA8998DSFHF', '2MONINA8998DSFHF');
+
+bidcoin.createNewTransaction(100, 'ADLAN3ONINA8998DSFHF', 'JULIA3MONINA8998DSFHF');
+bidcoin.createNewTransaction(200, 'ADLAN4ONINA8998DSFHF', 'JULIA4MONINA8998DSFHF');
+bidcoin.createNewTransaction(300, 'ADLAN5ONINA8998DSFHF', 'JULIA5MONINA8998DSFHF');
+
+bidcoin.createNewBlock(899999, '3ONINA8998DSFHF', '3MONINA8998DSFHF');
+
+console.log(bidcoin.chain[2]);
