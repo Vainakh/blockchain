73ebf0e3a39dbc13938e65d122b996aa240fe034
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-03-25 23:12:04 -0700

    Add transaction endpoint and add new transaction to the array

--- dev/api.js
+++ dev/api.js
@@ -14,8 +14,8 @@ app.get('/blockchain', (req, res) => {
 });
 
 app.post('/transaction', (req, res) => {
-  console.log(req.body);
-  res.send(`The amount of the transaction is ${req.body.amount} bidcoins.`)
+  const blockIndex = bidcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.json({ note: `Transaction will be added in block ${blockIndex}.`})
 });
 
 app.get('/mine', (req, res) => {
